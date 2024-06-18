97b7fba566915b3e8d3caadf0682fa869ae39323
Samuel <spietersma@gmail.com>
2024-06-18 13:04:26 -0400

    Added repnotify to AttributeSet (AS), added boilerplate accessors and default values to gameplay attribute data items

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,57 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(25.f);
+	InitMaxMana(100.f);
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+
+	//Repnotify ALWAYS vs Onchanged:
+	// If the value is the same as it was before, the default REPNOTIFY_OnChanged will not replicate the change
+	// However with REPNOTIFY_Always - it will. This is good because if we make a change to a variable that we want
+	// to respond to no matter what (aka 'if you receive healing, do xyx') then it will be replicated even if
+	// the health is max, for example
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	// input parameter for rep notify is always the old value, if provided!
+
+	// I believe the repnotify here is for GAS to be able to predicatively modify
+	// values - so if you click a spell, it will subtract 1 mana client side before
+	// the server has a chance to verify whether the spell cast was legal, and THEN the server
+	// will validate the cast and proceed. Maybe a bad example but eh
+	
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxMana);
+}
