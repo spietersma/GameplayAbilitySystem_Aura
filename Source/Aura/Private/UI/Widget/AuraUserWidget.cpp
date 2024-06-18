600c52a515ce974e6fa4d06b12461e15c49a33e2
Samuel <spietersma@gmail.com>
2024-06-18 15:30:10 -0400

    Added basic 'c' portion of MVC

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Scared and Confused
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
